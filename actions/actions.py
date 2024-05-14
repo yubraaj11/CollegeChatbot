9f17663e3f41d4ead2bcb0c7b072ef9527e76871
Yubraj Sigde <yubrajsigdel1@gmail.com>
2024-05-14 16:53:57 +0545

    Final Version

--- actions/actions.py
+++ actions/actions.py
@@ -150,3 +150,16 @@ class ActionAskSyllabus(Action):
             conn.close()
 
         return []
+
+# Default Action for Fallback Policy
+class ActionDefaultFallback(Action):
+    def name(self) -> Text:
+        return "action_default_fallback"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        dispatcher.utter_message(text="Sorry! I am unable to understand you. Please try again.")
+
+        return []
